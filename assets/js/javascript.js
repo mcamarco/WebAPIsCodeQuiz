f81c74828a3b2d7075d16af2d759877ea4fa46d5
Mickey Camarco <48422179+mcamarco@users.noreply.github.com>
2023-03-28 00:28:11 -0400

    added HTML template from 26 - Stu_Local_Storage-ToDos and linked CSS to HTML

---
+++ assets/js/javascript.js
@@ -0,0 +1,33 @@
+// User Story
+
+// AS A coding boot camp student
+// I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+// SO THAT I can gauge my progress compared to my peers
+
+// Acceptance Criteria
+
+// Click start button and timer starts as a question is presented
+// Once a question is answered, show if its correct or wrong then automatically move to the next question.
+// If a question is answered incorrectly, time is subtracted from the clock.
+// When all the questsion are answered or the time reaches 0, the game is over.
+// biggest mistake people make, is how you set up your 
+
+// example:
+
+// varshowBizPizza = []
+// var pizza = {
+//     toppings: [],
+//     size: "big"
+// }
+
+
+// TODO: Set Variable for question # and set variables for questions and arrays as the answers?
+var submittedanswer = []
+var questions = {
+    q1 = ["answer1",", answer 2",", answer 3",", answer 4"],
+    q2 = ["answer1",", answer 2",", answer 3",", answer 4"],
+    q3 = ["answer1",", answer 2",", answer 3",", answer 4"],
+    q4 = ["answer1",", answer 2",", answer 3",", answer 4"],
+    q5 = ["answer1",", answer 2",", answer 3",", answer 4"],
+    q6 = ["answer1",", answer 2",", answer 3",", answer 4"]
+}
