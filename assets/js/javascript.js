11be18a41abde78864500fcbc5756269892347ad
Mickey Camarco <48422179+mcamarco@users.noreply.github.com>
2023-03-28 17:18:01 -0400

    cleaning up comments and formatting

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -7,34 +7,30 @@
 // Click start button and timer starts as a question is presented
 
 
-
-
-
 // set questions
 var allQuestions = [
 
-    {  // Question 1
+    {  // Question 1 -- index=0
         question: ["Commonly used data types DO NOT include:"],
         answers: ['strings', 'booleans', 'alerts', 'numbers'],
         correctAnswer: 'alerts - correctAnswer1'
     },
 
-    {  // Question 2
+    {  // Question 2 -- index=1
         question: "The condition in an if / else statement is enclosed within _______.",
         answers: ['quotes', 'curly brackets', 'parenthesis', 'square brackets'],
         correctAnswer: 'correctAnswer2'
     },
 
 
-    {  // Question 3
+    {  // Question 3 -- index=2
         question: "String values must be enclosed within ______ when being assigned to variables",
         answers: ['commas', 'curly brackets', 'quotes', 'parentheses'],
         correctAnswer: 'correctAnswer3'
     },
 
 
-
-    { // Question 4
+    { // Question 4 -- index=3
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
         answers: ['JavaScript', 'terminal/bash', 'for loops', 'console.log'],
         correctAnswer: 'correctAnswer4'
