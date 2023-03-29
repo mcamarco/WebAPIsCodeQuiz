0e017e669945f0f1a82c3c9821574af54c7d4888
Mickey Camarco <48422179+mcamarco@users.noreply.github.com>
2023-03-28 23:51:26 -0400

    updating JS

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -32,91 +32,102 @@ var allQuestions = [
 var counter = 0;
 var time = 90;
 var timerId;
-var scoretotal;
+var scoretotal = 0;
 
 
 // set timer once start button is clicked to 1 minute
-function timer(){
-    timerId = setInterval(function(){
+function timer() {
+    timerId = setInterval(function () {
         document.getElementById("timer").textContent = time;
         time--;
-        if(time === 0){
+        if (time === 0) {
             finalscore();
         }
     }, 1000);
 }
 
 // hide questions, show final score and initials field
-function finalscore(){
+function finalscore() {
     clearInterval(timerId);
     //hide questions div
-    document.getElementById("questions").style.display='none';
-    
+    document.getElementById("questions").style.display = 'none';
+
     //show final score div
+    var finalScoreEl = document.getElementById("score")
+    finalScoreEl.textContent = `Final Score: ${scoretotal}`
+
 
     //save initials into local storage
-    
+    var inputVal = document.getElementById("initials").value;
+    console.log(inputVal);
+    var submitbtn = document.getElementById("submit")
+    submitbtn.addEventListener('click', () => {
+        var leaderboardString = inputVal + ' - ' + scoretotal + '/4'
+        console.log(leaderboardString)
+    } 
+    )
+
     // show results page including your score and request for initials, hide next show submit
 
     // show leaderboard with prior 3 submissions, hide submit show try again button
 }
 
-function checkAnswer(){
+function checkAnswer() {
     var userAns = this.getAttribute("data-text");
     console.log(userAns);
-    
+
     // CORRECT ANSWER
-    if(userAns === allQuestions[counter].correctAnswer)
-    {
+    if (userAns === allQuestions[counter].correctAnswer) {
         // change from alert to highlighting correct answer and red if wrong
         alert('Correct');
         // add one to counter
         counter++;
-        
+        console.log(counter)
+        scoretotal++
+        console.log(scoretotal)
         // if counter is greater than <4 then show the next question
-        if(counter < 4){
-        displayQuestion()
+        if (counter < 4) {
+            displayQuestion()
         }
         // if counter is <4 then show final score and initials field
-        else{
+        else {
             finalscore();
             console.log(finalscore)
         }
     }
 
     // INCORRECT ANSWER
-    else{
+    else {
         // change from alert to highlighting correct answer and red if wrong
         alert("Incorrect");
         time = time - 5;
         counter++;
-        if(counter < 4){
-        displayQuestion()
+        if (counter < 4) {
+            displayQuestion()
         }
-        else{
+        else {
             // function final score & initials
             finalscore();
         }
     }
 }
 
-function displayQuestion()
-{
+function displayQuestion() {
     document.getElementById("questions").textContent = "";
     var h4 = document.createElement("h4");
     h4.textContent = allQuestions[counter].question;
     document.getElementById("questions").append(h4);
 
-    for(var i=0; i<allQuestions[counter].answers.length;i++){
+    for (var i = 0; i < allQuestions[counter].answers.length; i++) {
         var btn = document.createElement("button");
         btn.textContent = allQuestions[counter].answers[i];
-        btn.setAttribute("data-text",allQuestions[counter].answers[i]);
+        btn.setAttribute("data-text", allQuestions[counter].answers[i]);
         btn.onclick = checkAnswer;
         document.getElementById("questions").append(btn);
     }
 }
 
-document.getElementById('start').addEventListener('click', function(){
+document.getElementById('start').addEventListener('click', function () {
     document.getElementById("introduction").style.display = "none";
     document.getElementById("questions");
     timer();
@@ -124,7 +135,8 @@ document.getElementById('start').addEventListener('click', function(){
 })
 
 
-// credit to doug in README :) 
-// credit tutor in README :) 
+// credit to doug in README :)
+// credit tutor in README :)
+// credit w3 schools
 
 
